c12cdfde9822d018e3324f0e993e48c02af9def8
Fotios <fotios@shadowrobot.com>
2019-01-25 14:21:08 +0000

    update

--- bin/run-ansible.sh
+++ bin/run-ansible.sh
@@ -72,8 +72,8 @@ echo ""
 
 pushd $aurora_home
 
-pip install --user -r data/requirements.txt
-~/.local/bin/ansible-playbook -vvv --ask-become-pass -i inventory/local "playbooks/${playbook}.yml" --extra-vars "$*"
+pip install --user -r ansible/data/requirements.txt
+~/.local/bin/ansible-playbook -vvv --ask-become-pass -i ansible/inventory/local "ansible/playbooks/${playbook}.yml" --extra-vars "$*"
 
 popd
 
