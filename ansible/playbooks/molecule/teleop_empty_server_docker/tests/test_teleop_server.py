82f6277fd1541524bf3e000dc1e16473bcfd213e
ToivoHn <toivo@shadowrobot.com>
2019-04-16 13:43:04 +0000

    first working test in teleop

--- ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
@@ -12,3 +12,8 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_docker_installed(host):
+    package = host.package('docker-ce')
+    assert package.is_installed
