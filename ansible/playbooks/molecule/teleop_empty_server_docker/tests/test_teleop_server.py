56f622bfa311772d214b663ee4162274cbd65147
ToivoHn <toivo@shadowrobot.com>
2019-05-13 13:22:42 +0000

    fix line too long

--- ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
@@ -47,6 +47,10 @@ def test_sr_config_exists_in_docker(host):
 
 def test_icons_in_docker(host):
     user = host.user().name
-    assert host.file('/home/'+str(user)+'/Desktop/ROS_Logs_Saver.desktop').exists
-    assert host.file('/home/'+str(user)+'/Desktop/Teleop_Control_Machine_Launch.desktop').exists
-    assert host.file('/home/'+str(user)+'/Desktop/Teleop_Server_Launch.desktop').exists
+    icon_list = []
+    desktop_path = '/home/'+str(user)+'/Desktop/'
+    icon_list.append(desktop_path+'ROS_Logs_Saver.desktop')
+    icon_list.append(desktop_path+'Teleop_Control_Machine_Launch.desktop')
+    icon_list.append(desktop_path+'Teleop_Server_Launch.desktop')
+    for icon in icon_list:
+        assert host.file(icon).exists
