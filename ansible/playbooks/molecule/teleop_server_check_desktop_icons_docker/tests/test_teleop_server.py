1a895eb26e8f7d9da34111e56900375c65353514
Fotios <fotios@shadowrobot.com>
2019-05-16 16:22:25 +0100

    updated base

--- ansible/playbooks/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
@@ -4,13 +4,14 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 def test_icons_in_docker(host):
     user = host.user().name
     icon_list = []
     script_list = []
     script_path = '/home/'+str(user)+'/Desktop/'
     desktop_path = '/home/'+str(user) + \
-                        '/.shadow_launcher_app/shadow_hand_launcher/'
+                   '/.shadow_launcher_app/shadow_hand_launcher/'
     icon_list.append(desktop_path +
                      'Teleop_Control_Machine_Launch_Demohand_A.desktop')
     icon_list.append(desktop_path +
