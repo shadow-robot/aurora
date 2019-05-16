9bfa3535329d6ebb8d95684368822e99b3984297
Toivohn <toivo@shadowrobot.com>
2019-05-16 11:57:32 +0000

    molecule tests pass on every scenario (apart from default_ec2)

--- ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
@@ -23,7 +23,7 @@ def test_docker_installed(host):
 def test_docker_container_exists(host):
     client = docker.from_env()
     try:
-        client.containers.get('dexterous_hand_real_hw')
+        client.containers.get('teleop')
         assert True
     except docker.errors.NotFound:
         assert False
@@ -33,13 +33,13 @@ def test_docker_container_exists(host):
 
 def test_correct_docker_image(host):
     client = docker.from_env()
-    image = str(client.containers.get('dexterous_hand_real_hw').image)
+    image = str(client.containers.get('teleop').image)
     assert image == "<Image: 'shadowrobot/dexterous-hand:kinetic-release'>"
 
 
 def test_sr_config_exists_in_docker(host):
     client = docker.from_env()
-    container = client.containers.get('dexterous_hand_real_hw')
+    container = client.containers.get('teleop')
     bits, stat = container.get_archive(
         '/home/user/projects/shadow_robot/base/src/sr_config')
     assert stat['size'] > 0
