3fc3fb98e3c6acd7c17a75186348bf97c179045f
Fotios <fotios@shadowrobot.com>
2019-05-17 12:12:35 +0100

    pr changes

--- ansible/playbooks/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
@@ -9,8 +9,8 @@ def test_icons_in_docker(host):
     user = host.user().name
     icon_list = []
     script_list = []
-    desktop_path = '/home/'+str(user)+'/Desktop/'
-    script_path = '/home/'+str(user) + \
+    desktop_path = '/home/' + str(user) + '/Desktop/'
+    script_path = '/home/' + str(user) + \
                    '/.shadow_launcher_app/shadow_hand_launcher/'
     icon_list.append(desktop_path +
                      'Teleop_Control_Machine_Launch_Demohand_A.desktop')
@@ -18,16 +18,16 @@ def test_icons_in_docker(host):
                      'Teleop_Control_Machine_Launch_Demohand_B.desktop')
     icon_list.append(desktop_path +
                      'Teleop_Control_Machine_Launch_Demohand_C.desktop')
-    icon_list.append(desktop_path+'Teleop_Container_Launch.desktop')
-    icon_list.append(desktop_path+'Teleop_GUI.desktop')
-    icon_list.append(desktop_path+'Teleop_ROSCORE.desktop')
+    icon_list.append(desktop_path + 'Teleop_Container_Launch.desktop')
+    icon_list.append(desktop_path + 'Teleop_GUI.desktop')
+    icon_list.append(desktop_path + 'Teleop_ROSCORE.desktop')
 
-    script_list.append(script_path+'teleop_exec_A.sh')
-    script_list.append(script_path+'teleop_exec_B.sh')
-    script_list.append(script_path+'teleop_exec_C.sh')
-    script_list.append(script_path+'shadow_launcher_exec.sh')
-    script_list.append(script_path+'shadow_roslaunch_demo.sh')
-    script_list.append(script_path+'shadow_roscore.sh')
+    script_list.append(script_path + 'teleop_exec_A.sh')
+    script_list.append(script_path + 'teleop_exec_B.sh')
+    script_list.append(script_path + 'teleop_exec_C.sh')
+    script_list.append(script_path + 'shadow_launcher_exec.sh')
+    script_list.append(script_path + 'shadow_roslaunch_demo.sh')
+    script_list.append(script_path + 'shadow_roscore.sh')
 
     for icon in icon_list:
         assert host.file(icon).exists
