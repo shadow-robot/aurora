ae93e7d4e86aec49335a7dbc69a0ec9bbd9d1ed3
Toivohn <toivo@shadowrobot.com>
2019-05-20 10:35:39 +0000

    fixed file path bug in test

--- ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule/teleop_empty_server_docker/tests/test_teleop_server.py
@@ -46,10 +46,26 @@ def test_sr_config_exists_in_docker(host):
 
 
 def test_icons_in_docker(host):
-    path = '/home/' + str(host.user().name) + '/Desktop/'
+    desktop_path = '/home/' + str(host.user().name) + '/Desktop/'
+    script_path = '/home/' + str(host.user().name) + \
+                  '/.shadow_launcher_app/shadow_hand_launcher/'
     icons = (
-        'ROS_Logs_Saver',
-        'Teleop_Control_Machine_Launch',
-        'Teleop_Server_Launch')
+        'Teleop_Control_Machine_Launch_Demohand_A',
+        'Teleop_Control_Machine_Launch_Demohand_B',
+        'Teleop_Control_Machine_Launch_Demohand_C',
+        'Teleop_Container_Launch',
+        'Teleop_GUI',
+        'Teleop_ROSCORE'
+        )
+    scripts = (
+        'teleop_exec_A',
+        'teleop_exec_B',
+        'teleop_exec_C',
+        'shadow_launcher_exec',
+        'shadow_roslaunch_demo',
+        'shadow_roscore'
+        )
     for icon in icons:
-        assert host.file(path+icon+'.desktop').exists
+        assert host.file(desktop_path+icon+'.desktop').exists
+    for script in scripts:
+        assert host.file(script_path+script+'.sh').exists
