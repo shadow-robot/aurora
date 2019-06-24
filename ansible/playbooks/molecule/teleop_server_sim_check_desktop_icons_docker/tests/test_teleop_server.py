0d6ced019f22db10b5ff40f201058c8609610981
Fotios <fotios.papadopoulos@plymouth.ac.uk>
2019-06-24 15:29:49 +0100

    adding tests

---
+++ ansible/playbooks/molecule/teleop_server_sim_check_desktop_icons_docker/tests/test_teleop_server.py
@@ -0,0 +1,33 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_icons_in_docker(host):
+    desktop_path = '/home/' + str(host.user().name) + '/Desktop/'
+    script_path = '/home/' + str(host.user().name) + \
+                  '/.shadow_launcher_app/shadow_hand_launcher/'
+    icons = (
+        'Teleop_control-machine_Launch_Demohand_A',
+        'Teleop_control-machine_Launch_Demohand_B',
+        'Teleop_control-machine_Launch_Demohand_C',
+        'Teleop_Container_Launch',
+        'Teleop_GUI',
+        'Teleop_ROSCORE',
+        'Teleop_Simulation'
+        )
+    scripts = (
+        'teleop_exec_A',
+        'teleop_exec_B',
+        'teleop_exec_C',
+        'shadow_launcher_exec',
+        'shadow_roslaunch_demo',
+        'shadow_roscore',
+        'shadow_sim_demo'
+        )
+    for icon in icons:
+        assert host.file(desktop_path+icon+'.desktop').exists
+    for script in scripts:
+        assert host.file(script_path+script+'.sh').exists
