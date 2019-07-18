b1851c9a5abfddfdb81c6b4064bd7172de4f37f2
ToivoHn <toivo@shadowrobot.com>
2019-07-18 16:37:52 +0000

    bug fixes

--- ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
@@ -9,13 +9,16 @@ def test_icons_in_docker(host):
     desktop_path = '/home/' + str(host.user().name) + '/Desktop/'
     script_path = '/home/' + str(host.user().name) + \
                   '/.shadow_launcher_app/shadow_hand_launcher/'
+    save_logs_script_path = '/home/' + str(host.user().name) + \
+                  '/.shadow_save_log_app/save_latest_ros_log/'
     icons = (
         'Teleop_control_machine_Launch_Demohand_A',
         'Teleop_control_machine_Launch_Demohand_B',
         'Teleop_control_machine_Launch_Demohand_C',
         'Teleop_Container_Launch',
         'Teleop_GUI',
-        'Teleop_ROSCORE'
+        'Teleop_ROSCORE',
+        'ROS_Logs_Saver_And_Uploader.desktop'
         )
     scripts = (
         'teleop_exec_A',
@@ -29,3 +32,5 @@ def test_icons_in_docker(host):
         assert host.file(desktop_path+icon+'.desktop').exists
     for script in scripts:
         assert host.file(script_path+script+'.sh').exists
+    save_logs_file = save_logs_script_path+'save-latest-ros-logs.sh'
+    assert host.file(save_logs_file).exists
