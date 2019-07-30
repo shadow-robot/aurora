a895e8cf21f463a56a24b1a2740677f2584040f6
azompas <anastasios@shadowrobot.com>
2019-07-30 15:04:00 +0000

    tutorial

---
+++ ansible/playbooks/molecule_docker/molecule/tutorial_1/tests/test_tutorial_1.py
@@ -0,0 +1,21 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_icons_in_docker(host):
+    desktop_path = '/home/' + str(host.user().name) + '/Desktop/'
+    script_path = '/home/' + str(host.user().name) + \
+                  '/.tutorial/tutorial_1/'
+    icons = (
+        'Launch_Tutorial_1'
+        )
+    scripts = (
+        'show_terminal'
+        )
+    for icon in icons:
+        assert host.file(desktop_path+icon+'.desktop').exists
+    for script in scripts:
+        assert host.file(script_path+script+'.sh').exists
