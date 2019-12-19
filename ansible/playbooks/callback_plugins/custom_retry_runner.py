2d5f0084f85eb9778208e3b55400bee932484e6c
giusebar <giuseppe@shadowrobot.com>
2019-12-19 16:41:48 +0000

    add coffee mug

--- ansible/playbooks/callback_plugins/custom_retry_runner.py
+++ ansible/playbooks/callback_plugins/custom_retry_runner.py
@@ -1,5 +1,3 @@
-# written for python2.7
-# Make coding more python3-ish
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -32,7 +30,9 @@ class CallbackModule_custom_retry_runner(CallbackModule_default):
         task_name = result.task_name or result._task
         if "pull" in result.task_name and "docker" in result.task_name.lower():
             msg = """                                      s
-                     Docker image pulling in progress... c[_]"""
+                     Docker image pulling in progress... c[_]
+
+                  """
             if (self._display.verbosity > 2 or '_ansible_verbose_always' in result._result) and '_ansible_verbose_override' not in result._result:
                 msg += "Result was: %s" % self._dump_results(result._result)
             self._display.display(msg, color=C.COLOR_DEBUG)
