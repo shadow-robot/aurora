e969ffc1e62bc636e11d21b5d270dbd95965da7b
giusebar <giuseppe@shadowrobot.com>
2019-12-19 16:38:53 +0000

    add coffee mug

--- ansible/playbooks/callback_plugins/custom_retry_runner.py
+++ ansible/playbooks/callback_plugins/custom_retry_runner.py
@@ -3,6 +3,8 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import time
+import sys
 from ansible import constants as C
 from ansible.playbook.task_include import TaskInclude
 from ansible.plugins.callback.default import CallbackModule as CallbackModule_default
@@ -23,12 +25,14 @@ class CallbackModule_custom_retry_runner(CallbackModule_default):
 
         self._play = None
         self._last_task_banner = None
+        self.animation = "|/-\\"
         super(CallbackModule, self).__init__()
 
     def v2_runner_retry(self, result):
         task_name = result.task_name or result._task
         if "pull" in result.task_name and "docker" in result.task_name.lower():
-            msg = "Docker image pulling in progress..."
+            msg = """                                      s
+                     Docker image pulling in progress... c[_]"""
             if (self._display.verbosity > 2 or '_ansible_verbose_always' in result._result) and '_ansible_verbose_override' not in result._result:
                 msg += "Result was: %s" % self._dump_results(result._result)
             self._display.display(msg, color=C.COLOR_DEBUG)
