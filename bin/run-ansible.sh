e8a122e583083e8299bd50890303114b4a26b4f1
giusebar <giuseppe@shadowrobot.com>
2019-12-20 10:33:13 +0000

    B fix debug msg ansible (#144)
    * Update main.yml
    
    * add custom callback plugin
    
    * add ansible cfg file
    
    * debug msg
    
    * Update run-ansible.sh
    
    * add callback path and debug flag
    
    * check my branch is run
    
    * fix typo
    
    * fix
    
    * fix
    
    * add other functions
    
    * fix bugs
    
    * make derivate class
    
    * fix import of module
    
    * fix
    
    * fix message
    
    * check case insensitive
    
    * check that cfg file is not needed
    
    * remove debug flag from sh script
    
    * Update custom_retry_runner.py
    
    * add coffee mug
    
    * add coffee mug
    
    * remove mug
    
    * remove crap
    
    * address comments

--- bin/run-ansible.sh
+++ bin/run-ansible.sh
@@ -80,6 +80,8 @@ echo "inventory    = ${aurora_inventory}"
 echo "limit        = ${aurora_limit}"
 
 export ANSIBLE_ROLES_PATH="${aurora_home}/ansible/roles"
+export ANSIBLE_CALLBACK_PLUGINS="/home/$USER/.ansible/plugins/callback:/usr/share/ansible/plugins/callback:${aurora_home}/ansible/playbooks/callback_plugins"
+export ANSIBLE_STDOUT_CALLBACK="custom_retry_runner"
 
 extra_vars=$*
 IFS=',' read -ra inputdata <<< "$read_input"
