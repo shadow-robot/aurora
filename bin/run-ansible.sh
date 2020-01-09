dab1555a6f1f3df6480f086c93842a8be14e604a
ToivoS <toivo@shadowrobot.com>
2020-01-09 13:02:47 +0000

    dhcp fix

--- bin/run-ansible.sh
+++ bin/run-ansible.sh
@@ -166,6 +166,20 @@ if [[ ! -f "${ansible_executable}" ]]; then
     ansible_executable=ansible-playbook
 fi
 
+#configure DHCP before running the actual playbook
+if [[ "${playbook}" = "server_and_nuc_deploy" ]]; then
+    echo ""
+    echo " ----------------------------------------------------------------------"
+    echo " |    Configuring DHCP network for server and nuc playbook            |"
+    echo " ----------------------------------------------------------------------"
+    echo ""
+    "${ansible_executable}" ${ansible_flags} -i "local" "ansible/playbooks/dhcp.yml" --extra-vars "$extra_vars"
+    echo ""
+    echo " ----------------------------------------------------------------------"
+    echo " |    DHCP network ready! Proceeding with server and nuc playbook      |"
+    echo " ----------------------------------------------------------------------"
+    echo ""
+fi
 "${ansible_executable}" ${ansible_flags} -i "${aurora_inventory}" "ansible/playbooks/${playbook}.yml" --extra-vars "$extra_vars"
 
 popd
