0664e8ca30dbb323c2043d2a1bf10d4f5ec84378
ToivoS <toivo@shadowrobot.com>
2020-02-03 18:07:07 +0000

    parallel tests

--- ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
@@ -18,6 +18,8 @@ def test_icons_in_docker(host):
         'Teleop_Container_Launch',
         'Teleop_GUI',
         'Teleop_ROSCORE',
+        'Teleop_Simulation',
+        'Teleop_Documentation',
         'Shadow ROS Logs Saver and Uploader'
         )
     scripts = (
@@ -26,7 +28,9 @@ def test_icons_in_docker(host):
         'teleop_exec_C',
         'shadow_launcher_exec',
         'shadow_roslaunch_demo',
-        'shadow_roscore'
+        'shadow_roscore',
+        'shadow_sim_demo',
+        'shadow_launcher_doc_exec'
         )
     for icon in icons:
         assert host.file(desktop_path+icon+'.desktop').exists
@@ -34,3 +38,19 @@ def test_icons_in_docker(host):
         assert host.file(script_path+script+'.sh').exists
     save_logs_file = save_logs_script_path+'save-latest-ros-logs.sh'
     assert host.file(save_logs_file).exists
+
+def test_openvpn_server_files(host):
+    openvpn_path = '/etc/openvpn/'
+
+    openvpn_files = (
+        'server.key',
+        'server.crt',
+        'ca.crt',
+        'ta.key',
+        'dh2048.pem'
+        )
+    for openvpn_file in openvpn_files:
+        assert host.file(openvpn_path + openvpn_file).exists
+    assert host.file(
+        '/home/' + str(host.user().name) +
+        '/openvpn-ca/teleop-client/teleop-client.ovpn').exists
