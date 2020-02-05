078df006a5ff66361e7a19434be7835d62903e04
ToivoS <38542384+ToivoS@users.noreply.github.com>
2020-02-05 09:30:15 +0000

    F#src 3994 automated tests server nuc playbook (#167)
    * automating dhcp
    
    * regexp
    
    * fixes
    
    * fix
    
    * test
    
    * fixes
    
    * fix
    
    * delay fix
    
    * hand_serial
    
    * fix
    
    * fix
    
    * attempt
    
    * xpath fix
    
    * fix
    
    * fix
    
    * proper xml parsing
    
    * syntax
    
    * fix
    
    * parallel tests
    
    * Delete buildspec.yml
    
    * lint fix
    
    * added proper tests
    
    * lint fix
    
    * lint fix
    
    * fixing bug
    
    * privileges
    
    * fix ansible
    
    * fixes
    
    * fixes
    
    * fix
    
    * fix
    
    * fix
    
    * retrigger
    
    * fix ansible variable redefinition issue
    
    * ssh folder ensure exists
    
    * fix
    
    * Update molecule.yml
    
    * Update molecule.yml
    
    * Create main.yml
    
    * Update install_software.yml
    
    * Update main.yml
    
    * Update main.yml

---
+++ ansible/playbooks/molecule_docker/molecule/server_and_nuc_desktop_icons_docker/tests/test_server.py
@@ -0,0 +1,52 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_chrony_server_installed(host):
+    file_path = "/etc/chrony/chrony.conf"
+    assert host.file(file_path).exists
+
+
+def test_icons_in_docker(host):
+    desktop_path = '/home/' + str(host.user().name) + '/Desktop/'
+    script_path = '/home/' + str(host.user().name) + \
+                  '/.shadow_launcher_app/shadow_hand_launcher/'
+    save_logs_script_path = '/home/' + str(host.user().name) + \
+                            '/.shadow_save_log_app/save_latest_ros_logs/'
+    icons = (
+        'Launch Shadow Hand',
+        'Shadow NUC RQT',
+        'Shadow Advanced Launchers/1 - Launch Server Container',
+        'Shadow Advanced Launchers/2 - Launch Server ROSCORE',
+        'Shadow Advanced Launchers/3 - Launch NUC Container ' +
+        'and Hardware Control Loop',
+        'Shadow Advanced Launchers/4 - Launch Server GUI',
+        'Shadow Advanced Launchers/Launch NUC Container',
+        'Shadow Demos/Close Right Hand',
+        'Shadow Demos/Demo Right Hand',
+        'Shadow Demos/Open Right Hand',
+        'Shadow Advanced Launchers/Launch Local Shadow Hand',
+        'Shadow ROS Logs Saver'
+        )
+    scripts = (
+        'shadow_launch_everything',
+        'nuc_rqt',
+        'shadow_server_container',
+        'shadow_roscore',
+        'shadow_nuc_hardware_control_loop',
+        'shadow_GUI',
+        'shadow_nuc_container',
+        'close_right_hand',
+        'demo_right_hand',
+        'open_right_hand',
+        'shadow_local_launcher_exec'
+        )
+    for icon in icons:
+        assert host.file(desktop_path+icon+'.desktop').exists
+    for script in scripts:
+        assert host.file(script_path+script+'.sh').exists
+    save_logs_file = save_logs_script_path+'save-latest-ros-logs.sh'
+    assert host.file(save_logs_file).exists
