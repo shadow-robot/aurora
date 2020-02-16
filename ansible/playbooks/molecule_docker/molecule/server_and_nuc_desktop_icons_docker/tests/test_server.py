506545a157fa39c110f3506cf7d1e1ef57e9ca67
ToivoS <toivo@shadowrobot.com>
2020-02-16 19:08:27 +0000

    molecule fix

--- ansible/playbooks/molecule_docker/molecule/server_and_nuc_desktop_icons_docker/tests/test_server.py
+++ ansible/playbooks/molecule_docker/molecule/server_and_nuc_desktop_icons_docker/tests/test_server.py
@@ -17,7 +17,9 @@ def test_icons_in_docker(host):
     save_logs_script_path = '/home/' + str(host.user().name) + \
                             '/.shadow_save_log_app/save_latest_ros_logs/'
     icons = (
-        'Launch Shadow Hand',
+        'Launch Shadow Right Hand',
+        'Launch Shadow Left Hand',
+        'Launch Shadow Bimanual Hands',
         'Shadow NUC RQT',
         'Shadow Advanced Launchers/1 - Launch Server Container',
         'Shadow Advanced Launchers/2 - Launch Server ROSCORE',
@@ -28,11 +30,18 @@ def test_icons_in_docker(host):
         'Shadow Demos/Close Right Hand',
         'Shadow Demos/Demo Right Hand',
         'Shadow Demos/Open Right Hand',
-        'Shadow Advanced Launchers/Launch Local Shadow Hand',
+        'Shadow Demos/Close Left Hand',
+        'Shadow Demos/Demo Left Hand',
+        'Shadow Demos/Open Left Hand',
+        'Shadow Advanced Launchers/Launch Local Shadow Right Hand',
+        'Shadow Advanced Launchers/Launch Local Shadow Left Hand',
+        'Shadow Advanced Launchers/Launch Local Shadow Bimanual Hands',
         'Shadow ROS Logs Saver'
         )
     scripts = (
-        'shadow_launch_everything',
+        'shadow_launch_everything_right',
+        'shadow_launch_everything_left',
+        'shadow_launch_everything_bimanual',
         'nuc_rqt',
         'shadow_server_container',
         'shadow_roscore',
@@ -42,7 +51,12 @@ def test_icons_in_docker(host):
         'close_right_hand',
         'demo_right_hand',
         'open_right_hand',
-        'shadow_local_launcher_exec'
+        'close_left_hand',
+        'demo_left_hand',
+        'open_left_hand',
+        'shadow_local_right_launcher_exec',
+        'shadow_local_left_launcher_exec',
+        'shadow_local_bimanual_launcher_exec'
         )
     for icon in icons:
         assert host.file(desktop_path+icon+'.desktop').exists
