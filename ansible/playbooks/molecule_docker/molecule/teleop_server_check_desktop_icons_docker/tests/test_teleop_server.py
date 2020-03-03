47e6d20a19e5a0684a7922de5956c5ea115716d8
ToivoS <toivo@shadowrobot.com>
2020-03-03 13:52:09 +0000

    fixed tests and docker image version

--- ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
@@ -12,25 +12,49 @@ def test_icons_in_docker(host):
     save_logs_script_path = '/home/' + str(host.user().name) + \
                             '/.shadow_save_log_app/save_latest_ros_logs/'
     icons = (
-        'Teleop_control_machine_Launch_Demohand_A',
-        'Teleop_control_machine_Launch_Demohand_B',
-        'Teleop_control_machine_Launch_Demohand_C',
-        'Teleop_Container_Launch',
-        'Teleop_GUI',
-        'Teleop_ROSCORE',
-        'Teleop_Simulation',
-        'Teleop_Documentation',
-        'Shadow ROS Logs Saver and Uploader'
+        'Launch Shadow Right Teleop',
+        'Shadow NUC RQT',
+        'Shadow Advanced Launchers/1 - Launch Server Container',
+        'Shadow Advanced Launchers/2 - Launch Server ROSCORE',
+        'Shadow Advanced Launchers/3 - Launch NUC Right ' +
+        'Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand A Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand B Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand C Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand D Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/4 - Launch Right Teleop GUI',
+        'Shadow Advanced Launchers/5 - Launch Right Polhemus Mapping',
+        'Shadow Advanced Launchers/6 - Launch Right Polhemus Driver',
+        'Shadow Advanced Launchers/Launch NUC Container',
+        'Shadow Demos/Close Right Hand',
+        'Shadow Demos/Demo Right Hand',
+        'Shadow Demos/Open Right Hand',
+        'Shadow ROS Logs Saver',
+        'Teleop Documentation',
         )
-    scripts = (
+    scripts = (   
+        'shadow_launch_right_teleop.sh',
+        'nuc_rqt',
+        'shadow_server_container',
+        'shadow_roscore',
+        'shadow_nuc_right_hardware_control_loop',
         'teleop_exec_A',
         'teleop_exec_B',
         'teleop_exec_C',
-        'shadow_launcher_exec',
-        'shadow_roslaunch_demo',
-        'shadow_roscore',
-        'shadow_sim_demo',
-        'shadow_launcher_doc_exec'
+        'teleop_exec_D',   
+        'shadow_GUI_right',
+        'shadow_polhemus_mapping_launch_right.sh',
+        'shadow_polhemus_driver_right.sh',
+        'shadow_nuc_container',
+        'close_right_hand',
+        'demo_right_hand',
+        'open_right_hand',
+        'shadow_launcher_doc_exec.sh',
+        'shadow_sim_demo.sh'
         )
     for icon in icons:
         assert host.file(desktop_path+icon+'.desktop').exists
--- ansible/playbooks/molecule_docker/molecule/teleop_server_desktop_icons_haptx_bimanual_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule_docker/molecule/teleop_server_desktop_icons_haptx_bimanual_docker/tests/test_teleop_server.py
@@ -28,19 +28,72 @@ def test_icons_in_docker(host):
     save_logs_script_path = '/home/' + str(host.user().name) + \
                             '/.shadow_save_log_app/save_latest_ros_logs/'
     icons = (
-        'Teleop_control_machine_Launch_bimanual_demohands_B_D',
-        'Teleop_Container_Launch',
-        'Teleop_GUI_haptx_Bimanual_Launch',
-        'Teleop_ROSCORE',
+        'Launch Shadow Right Teleop',
+        'Launch Shadow Left Teleop',
+        'Launch Shadow Bimanual Teleop',
+        'Shadow NUC RQT',
+        'Shadow Advanced Launchers/1 - Launch Server Container',
+        'Shadow Advanced Launchers/2 - Launch Server ROSCORE',
+        'Shadow Advanced Launchers/3 - Launch NUC Right ' +
+        'Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Launch NUC Left ' +
+        'Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Launch NUC Bimanual ' +
+        'Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand A Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand B Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand C Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/3 - Demohand D Launch NUC ' +
+        'Right Side Teleop Hardware Control Loop',
+        'Shadow Advanced Launchers/4 - Launch Right Teleop GUI',
+        'Shadow Advanced Launchers/4 - Launch Left Teleop GUI',
+        'Shadow Advanced Launchers/4 - Launch Bimanual Teleop GUI',
+        'Shadow Advanced Launchers/5 - Launch Right HaptX Mapping',
+        'Shadow Advanced Launchers/5 - Launch Left HaptX Mapping',
+        'Shadow Advanced Launchers/5 - Launch Bimanual HaptX Mapping',
+        'Shadow Advanced Launchers/Launch NUC Container',
+        'Shadow Demos/Close Right Hand',
+        'Shadow Demos/Demo Right Hand',
+        'Shadow Demos/Open Right Hand',
+        'Shadow Demos/Close Left Hand',
+        'Shadow Demos/Demo Left Hand',
+        'Shadow Demos/Open Left Hand',
         'Shadow ROS Logs Saver',
-        'Teleop_Haptx_Mapping_Bimanual_Launch'
+        'Teleop Documentation',
+        'Bimanual HaptX Teleop Simulation'
         )
-    scripts = (
-        'teleop_exec_bimanual',
-        'shadow_launcher_exec',
-        'shadow_roslaunch_bimanual',
+    scripts = (   
+        'shadow_launch_right_teleop.sh',
+        'shadow_launch_left_teleop.sh',
+        'shadow_launch_bimanual_teleop.sh',
+        'nuc_rqt',
+        'shadow_server_container',
         'shadow_roscore',
-        'shadow_haptx_mapping_launch_bimanual'
+        'shadow_nuc_right_hardware_control_loop',
+        'shadow_nuc_left_hardware_control_loop',
+        'shadow_nuc_bimanual_hardware_control_loop',
+        'teleop_exec_A',
+        'teleop_exec_B',
+        'teleop_exec_C',
+        'teleop_exec_D',   
+        'shadow_GUI_left',
+        'shadow_GUI_right',
+        'shadow_GUI_bimanual',
+        'shadow_haptx_mapping_launch_right.sh',
+        'shadow_haptx_mapping_launch_left.sh',
+        'shadow_haptx_mapping_launch_bimanual.sh',
+        'shadow_nuc_container',
+        'close_right_hand',
+        'demo_right_hand',
+        'open_right_hand',
+        'close_left_hand',
+        'demo_left_hand',
+        'open_left_hand',
+        'shadow_launcher_doc_exec.sh',
+        'shadow_sim_demo.sh'
         )
     for icon in icons:
         assert host.file(desktop_path+icon+'.desktop').exists
