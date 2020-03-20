ca1bfe3d0041cce70dd01b6621b15774e66bc25b
ToivoS <toivo@shadowrobot.com>
2020-03-20 18:28:36 +0000

    consolidated tests

--- ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
+++ ansible/playbooks/molecule_docker/molecule/teleop_server_check_desktop_icons_docker/tests/test_teleop_server.py
@@ -5,6 +5,44 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_docker_installed(host):
+    package = host.package('docker-ce')
+    assert package.is_installed
+
+
+def test_docker_container_exists(host):
+    client = docker.from_env()
+    try:
+        client.containers.get('teleop')
+        assert True
+    except docker.errors.NotFound:
+        assert False
+    except docker.errors.APIError:
+        assert False
+
+
+def test_correct_docker_image(host):
+    client = docker.from_env()
+    image = str(client.containers.get('teleop').image)
+    assert image == "<Image: 'shadowrobot/dexterous-hand:melodic-release'>"
+
+
+def test_sr_config_exists_in_docker(host):
+    client = docker.from_env()
+    container = client.containers.get('teleop')
+    bits, stat = container.get_archive(
+        '/home/user/projects/shadow_robot/base/src/sr_config')
+    assert stat['size'] > 0
+
+
 def test_icons_in_docker(host):
     desktop_path = '/home/' + str(host.user().name) + '/Desktop/'
     script_path = '/home/' + str(host.user().name) + \
