2db308a915af2d02f24cd083abfb6532f098c010
ToivoS <toivo@shadowrobot.com>
2020-08-18 12:40:15 +0000

    fix

--- bin/run-ansible.sh
+++ bin/run-ansible.sh
@@ -117,7 +117,7 @@ do
     value="${extra_var#*=}"
     if [[ "$variable" == "glove" ]]; then
         allowed_values="haptx shadow_glove cyberglove"
-        if ! "$value" in "$allowed_values"; then
+        if ! [[ "$value" in "$allowed_values" ]]; then
             echo "Variable $variable has invalid value $value"
             echo ""
             echo "The allowed values for $variable are: $allowed_values"
