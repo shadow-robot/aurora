1ab05a9190868e05c1504618ed8eec4492ce5e10
Viktor Madarasz <viktor@shadowrobot.com>
2020-10-26 19:32:24 +0000

    cleanup

---
+++ ansible/roles/installation/coronabar/files/coronabar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Deploy Coronabar Snap Application"
+sudo snap install coronabar
+echo "Coronabar Snap App has been installed."
--- ansible/roles/installation/mplabx/files/mplabx.sh
+++ ansible/roles/installation/mplabx/files/mplabx.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 echo "Deploy MPLABX IDE and MPLABX IPE and Go Through the Installer"
-sudo apt-get -y install wget
-wget https://ww1.microchip.com/downloads/en/DeviceDoc/MPLABX-v5.45-linux-installer.tar
-echo "where does it download the file"
-pwd
-echo "where does it download the file"
-tar -xvf MPLABX-v5.45-linux-installer.tar -C .
-sudo chmod +x MPLABX-v5.45-linux-installer.sh
-sudo $PWD/MPLABX-v5.45-linux-installer.sh
-wait
-echo "MPLABX IDE and MPLABX IPE staging is done. Installer will continue in a new window."
+sudo ./MPLABX-v5.45-linux-installer.run --mode unattended
+echo "MPLABX IDE and MPLABX IPE staging is done. Installer will continue unattended."
