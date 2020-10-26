286614afff32c6c4f6378d743664f520b15f6c9e
Viktor Madarasz <viktor@shadowrobot.com>
2020-10-26 14:12:04 +0000

    commit pushed from sh script VM

--- ansible/roles/installation/mplabx/files/mplabx.sh
+++ ansible/roles/installation/mplabx/files/mplabx.sh
@@ -2,7 +2,7 @@
 echo "Deploy MPLABX IDE and MPLABX IPE and Go Through the Installer"
 sudo apt-get -y install wget
 wget https://ww1.microchip.com/downloads/en/DeviceDoc/MPLABX-v5.45-linux-installer.tar
-tar -xv MPLABX-v5.45-linux-installer.tar -C .
+tar -xvf MPLABX-v5.45-linux-installer.tar -C .
 sudo chmod +x MPLABX-v5.45-linux-installer.sh
 sudo ./MPLABX-v5.45-linux-installer.sh
 echo "MPLABX IDE and MPLABX IPE staging is done. Installer will continue in a new window."
