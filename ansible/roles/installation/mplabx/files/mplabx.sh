1e18b47aa7196edee7e01f1c68c494322f5b69b2
Viktor Madarasz <viktor@shadowrobot.com>
2020-10-26 14:54:23 +0000

    commit pushed from sh script VM

--- ansible/roles/installation/mplabx/files/mplabx.sh
+++ ansible/roles/installation/mplabx/files/mplabx.sh
@@ -7,5 +7,5 @@ pwd
 echo "where does it download the file"
 tar -xvf MPLABX-v5.45-linux-installer.tar -C .
 sudo chmod +x MPLABX-v5.45-linux-installer.sh
-sudo ./MPLABX-v5.45-linux-installer.sh
+sudo ./tmp/aurora/ansible/playbooks/MPLABX-v5.45-linux-installer.sh
 echo "MPLABX IDE and MPLABX IPE staging is done. Installer will continue in a new window."
