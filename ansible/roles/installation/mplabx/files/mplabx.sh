e1eaee3f3300a770ce4c201a252723257094c4c5
Viktor Madarasz <viktor@shadowrobot.com>
2020-10-27 14:33:18 +0000

    commit pushed from sh script VM

--- ansible/roles/installation/mplabx/files/mplabx.sh
+++ ansible/roles/installation/mplabx/files/mplabx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Deploy MPLABX IDE and MPLABX IPE and Go Through the Installer"
 wget -P /home/user/Desktop/ https://s3.eu-west-2.amazonaws.com/com.shadowrobot.eu-west-2.public/public_aurora_files/MPLABX-v5.45-linux-installer.run
+sudo chmod +x /home/user/Desktop/MPLABX-v5.45-linux-installer.run
 sudo /home/user/Desktop/MPLABX-v5.45-linux-installer.run --mode unattended
 echo "MPLABX IDE and MPLABX IPE staging is done. Installer will continue unattended."
