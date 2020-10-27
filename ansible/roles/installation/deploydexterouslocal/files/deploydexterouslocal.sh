9026dbd227c51d060b5962232b4bc14c98c29a8a
Viktor Madarasz <viktor@shadowrobot.com>
2020-10-27 12:12:50 +0000

    commit pushed from sh script VM

--- ansible/roles/installation/deploydexterouslocal/files/deploydexterouslocal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Deploy Dexterous Local Deployment"
-sudo echo "nothing here yet"
-echo "Deploy Dexterous Hand Local Deployment is done"
--- ansible/roles/installation/mplabx/files/mplabx.sh
+++ ansible/roles/installation/mplabx/files/mplabx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Deploy MPLABX IDE and MPLABX IPE and Go Through the Installer"
-sudo ./MPLABX-v5.45-linux-installer.run --mode unattended
+sudo /home/user/Desktop/MPLABX-v5.45-linux-installer.run --mode unattended
 echo "MPLABX IDE and MPLABX IPE staging is done. Installer will continue unattended."
