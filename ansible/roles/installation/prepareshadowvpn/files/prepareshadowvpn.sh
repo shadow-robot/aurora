be2e289ef84b93356b338af7baf5eac3a69eb64a
viktor-shadowrobot <viktor@shadowrobot.com>
2020-11-02 09:49:55 +0000

    Commit ran from Terminal - VM

--- ansible/roles/installation/prepareshadowvpn/files/prepareshadowvpn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Preparing Environment for ShadowRobot VPN"
-sudo add-apt-repository -y ppa:nm-l2tp/network-manager-l2tp
-sudo apt-get -y update
-sudo apt-get -y install network-manager-l2tp-gnome
-sudo service xl2tpd stop
-sudo update-rc.d xl2tpd disable
-echo "Please Log Out and Log Back in to Continue Shadow Robot VPN Configuration"
--- ansible/roles/installation/resolvconf/files/resolvconf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Deploy Resolvconf to fix issue with Internet Connectivity Loss on 18.04 LTS"
-sudo apt-get -y install resolvconf
-echo "Resolvconf Deployment is done"
