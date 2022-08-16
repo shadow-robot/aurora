59dd691da2d3753319f9fbaf7e0af9db89343d69
Ben Starmer-Smith <ben@shadowrobot.com>
2022-08-16 10:13:43 +0100

    Added source for roscommands.

--- ansible/roles/products/common/hand_config/scripts/update_hand_config.sh
+++ ansible/roles/products/common/hand_config/scripts/update_hand_config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Used to update hand config.
-roscd sr_hand_config
+source /home/user/projects/shadow_robot/base/devel/setup.bash
+roscd sr_hand_config & cd ..
 git remote set-url origin https://github.com/shadow-robot/sr_hand_config
 git pull  
 cd ~
