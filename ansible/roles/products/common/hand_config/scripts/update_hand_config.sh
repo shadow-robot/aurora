e6bc83f895ff065c20ea80f06cee28a6a1a58c15
Ben Starmer-Smith <ben@shadowrobot.com>
2022-08-16 10:20:59 +0100

    Updated Script for container.

--- ansible/roles/products/common/hand_config/scripts/update_hand_config.sh
+++ ansible/roles/products/common/hand_config/scripts/update_hand_config.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Used to update hand config.
-source /home/user/projects/shadow_robot/base/devel/setup.bash
-roscd sr_hand_config & cd ..
+cd ~/projects/shadow_robot/base_deps/src/sr_hand_config
 git remote set-url origin https://github.com/shadow-robot/sr_hand_config
 git pull  
 cd ~
