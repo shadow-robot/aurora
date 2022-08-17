def7584a87f7d407e0042238fb80d53596d83fa4
Ben Starmer-Smith <ben@shadowrobot.com>
2022-08-17 15:54:43 +0100

    Removed un-needed script folder.

--- ansible/roles/products/common/hand_config/scripts/update_hand_config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Used to update hand config.
-cd ~/projects/shadow_robot/base_deps/src/sr_hand_config
-git remote set-url origin https://github.com/shadow-robot/sr_hand_config
-git pull  
-cd ~
