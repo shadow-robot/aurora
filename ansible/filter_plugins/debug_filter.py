9e9d74c883534a4747ff5858ca9929a317e0332f
Ben Starmer-Smith <ben@shadowrobot.com>
2023-06-06 09:31:49 +0100

    Added multiple fail messages with ignore_errors to make it easier to read errors.

--- ansible/filter_plugins/debug_filter.py
+++ ansible/filter_plugins/debug_filter.py
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 Shadow Robot Company Ltd.
+# Copyright 2023 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
@@ -12,6 +12,12 @@
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
----
-ubuntu_version: "18.04"
-skip_nvidia: false
+from ansible.utils.display import Display
+
+
+class FilterModule(object):
+    def filters(self): return {'warn_filter': self.warn_filter}
+
+    def warn_filter(self, message, **kwargs):
+        Display().warning(message)
+        return message
