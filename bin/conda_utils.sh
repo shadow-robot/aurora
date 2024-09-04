0810fb6bd65a7872f34c8e1daffaf0371e757cf2
Tom Queen <tom_q@hotmail.co.uk>
2024-09-04 16:19:25 +0100

    Update conda_utils.sh

--- bin/conda_utils.sh
+++ bin/conda_utils.sh
@@ -111,14 +111,13 @@ deploy_conda_installer() {
 }
 
 create_conda_ws(){
-  # deploy_conda_installer
+  deploy_conda_installer
   shadow_conda_ws_dir="${miniconda_install_location}/envs/${conda_ws_name}"
   if [ -d "$shadow_conda_ws_dir" ]; then
     rm -rf $shadow_conda_ws_dir
   fi
-  # ${miniconda_install_location}/bin/conda create -y -n ${conda_ws_name} python=3.8 && source ${miniconda_install_location}/bin/activate ${conda_ws_name}
-  source ${miniconda_install_location}/bin/activate ${conda_ws_name}
-  # python -m pip install yq xq
+  ${miniconda_install_location}/bin/conda create -y -n ${conda_ws_name} python=3.8 && source ${miniconda_install_location}/bin/activate ${conda_ws_name}
+  python -m pip install yq xq
 }
 
 fetch_pip_files(){ _fetch_new_files "http://shadowrobot.aurora-host-packages-${codename}.s3.eu-west-2.amazonaws.com" "pip_packages"; }
