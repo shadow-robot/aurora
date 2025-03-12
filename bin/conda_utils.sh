0e48a64a8fd1437cc3ade0cb47dcc71496ed9880
George E-Green <112101154+Athen-Player1@users.noreply.github.com>
2025-03-12 15:48:30 +0000

    update conda_util

--- bin/conda_utils.sh
+++ bin/conda_utils.sh
@@ -37,10 +37,8 @@ while IFS= read -r line; do
 done < <(lsb_release -a 2>/dev/null)
 
 # We use this variable to figure out which pip packages to download. Packages for focal work on jammy, but bionic needs its own packages
-if [[ $codename == *"bionic"* ]]; then
-  codename="bionic" 
-else 
-  codename="focal"
+if ! [[ $codename == *"bionic"* ]]; then
+  codename="focal" 
 fi
 
 _fetch_conda_installer() {
