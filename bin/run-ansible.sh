1f05012ffd70136717fbcea22a04f784d0fa0177
Nikolaus Holmes <143398822+niko-holmes@users.noreply.github.com>
2026-08-06 09:23:59 +0100

    Fix pip package printing

--- bin/run-ansible.sh
+++ bin/run-ansible.sh
@@ -504,7 +504,7 @@ if ! install_pip_packages >> "$LOG_FILE" 2>&1; then
     show_error "Failed to install pip packages using install_pip_packages. Check $LOG_FILE."
 fi
 log_message "Pip packages installed."
-log_message "$(pip list)"
+log_message "$(list_pip_packages)"
 
 
 # Fix for WSL
